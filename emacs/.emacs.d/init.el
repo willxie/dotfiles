d75378a9a991735a43abc9868b81a4955549ed72
William Xie <wxie@cs.utexas.edu>
2016-10-07 20:15:07 -0700

    Added MELP for emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -6,6 +6,15 @@
 ; line beginning with four semicolons; and each
 ; entry is introduced by a line beginning with
 ; three semicolons.
+
+; MELPA
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
+
 (package-initialize)
 (add-to-list 'load-path "~/.emacs.d/elisp/")
 
